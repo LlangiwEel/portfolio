593f4c05156da58152ff49c0bf1dc0c7ea175dd8
Lee Wignall <lee.j.wignall@gmail.com>
2018-10-17 16:22:37 +0100

    add technology elements

--- src/App.js
+++ src/App.js
@@ -6,9 +6,15 @@ class App extends Component {
     return (
       <div className="App">
       <header>
-      <h1>Lee Wignall</h1>
-      <p>Front-end Web Developer</p>
-      
+        <h1>Lee Wignall</h1>
+        <p>Front-end Web Developer</p>
+          <table>
+          <td>Javascript ES6</td>
+          <td>SCSS</td>
+          <td>HTML</td>
+          <td>React</td>
+          <td>Git</td>
+          </table>
       </header>
       </div>
     );
